643f5f4154ded889bff15c3bf7f738f2c1aa327a
litienan <hitienan@126.com>
2017-11-11 20:44:43 +0800

    test

---
+++ app/src/main/java/com/main/automarket/MainActivity.java
@@ -0,0 +1,16 @@
+package com.main.automarket;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(main.market.com.automarket.R.layout.activity_main);
+
+        //ddd
+    }
+
+}
