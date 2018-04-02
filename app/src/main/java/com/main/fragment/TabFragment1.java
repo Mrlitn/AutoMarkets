3046fb37ce72b47dfb88630e066f8706c1c49562
litienan <hitienan@126.com>
2018-04-02 18:39:40 +0800

    test

--- app/src/main/java/com/main/fragment/TabFragment1.java
+++ app/src/main/java/com/main/fragment/TabFragment1.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.main.activity.R;
 import com.main.base.BaseFragment;
@@ -15,13 +16,21 @@ import com.main.base.BaseFragment;
 
 public class TabFragment1 extends BaseFragment {
     private View view;
+    private TextView center_name;
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment1, null);
+
+        initView();
         return view;
+    }
+
+    private void initView() {
+        center_name = view.findViewById(R.id.center_name);
+        center_name.setText("车易行");
     }
 
 
--- app/src/main/java/com/main/fragment/TabFragment2.java
+++ app/src/main/java/com/main/fragment/TabFragment2.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.main.activity.R;
 import com.main.base.BaseFragment;
@@ -15,13 +16,21 @@ import com.main.base.BaseFragment;
 
 public class TabFragment2 extends BaseFragment {
     private View view;
+    private TextView tv;
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment2, null);
+
+        initView();
         return view;
+    }
+
+    private void initView() {
+        view.findViewById(R.id.select_city).setVisibility(View.INVISIBLE);
+        view.findViewById(R.id.search_cars).setVisibility(View.INVISIBLE);
     }
 
 
